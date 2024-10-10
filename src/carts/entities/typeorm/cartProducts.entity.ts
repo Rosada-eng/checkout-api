8d561aeae31dbdcefe9dc68e080fbfaf5649b392
Guilherme Rosada <guilhermer4@al.insper.edu.br>
2024-10-10 12:53:16 +0100

    feat(CartProducts): implements typeorm entity

---
+++ src/carts/entities/typeorm/cartProducts.entity.ts
@@ -0,0 +1,39 @@
+import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, OneToOne, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+import { ICartProducts } from '../cartProducts.model';
+import { Cart } from './cart.entity';
+import { Product } from 'src/products/entities/typeorm/product.entity';
+
+@Entity('cart_products')
+export class CartProducts implements ICartProducts {
+    @PrimaryGeneratedColumn("uuid")
+    id: string
+
+    @Column()
+    cartId: string
+
+    @OneToOne(() => Cart)
+    @JoinColumn({
+        name: 'cartId',
+        referencedColumnName: 'id',
+        foreignKeyConstraintName: 'FK_cart_products_cart'
+    })
+    cart: Cart
+
+    @Column()
+    productId: string
+
+    @ManyToOne(() => Product, (product) => product.id)
+    product: Product
+
+    @Column({ default: 1 })
+    amount: number
+
+    @Column()
+    unitPriceCents: number
+
+    @CreateDateColumn()
+    createdAt: Date
+
+    @UpdateDateColumn()
+    updatedAt: Date
+}
