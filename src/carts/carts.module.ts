8815eb0f69cdb602922f9d004107ece45e0afe24
Guilherme Rosada <guilhermer4@al.insper.edu.br>
2024-10-10 21:12:42 +0100

    edit(Cart): edit module importation

--- src/carts/carts.module.ts
+++ src/carts/carts.module.ts
@@ -6,17 +6,26 @@ import { Cart } from './entities/typeorm/cart.entity';
 import { CartProducts } from './entities/typeorm/cartProducts.entity';
 import { CartRepository } from './repositories/typeorm/cart.repository';
 import { ProductsModule } from 'src/products/products.module';
-import { ProductRepository } from 'src/products/repositories/product.repository';
+import { ProductRepository } from 'src/products/repositories/typeorm/product.repository';
+import { CompaniesModule } from 'src/companies/companies.module';
+import { CompanyRepository } from 'src/companies/repositories/typeorm/company.repository';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([
-    Cart,
-    CartProducts
-  ]),
-  ProductsModule
+  imports: [
+    TypeOrmModule.forFeature([
+      Cart,
+      CartProducts
+    ]),
+    ProductsModule,
+    CompaniesModule
 ],
   controllers: [CartsController],
-  providers: [CartRepository, ProductRepository, CartUseCases],
+  providers: [
+    CartRepository, 
+    ProductRepository, 
+    CompanyRepository, 
+    CartUseCases
+],
   exports: [CartUseCases, TypeOrmModule],
 })
 export class CartsModule {}
