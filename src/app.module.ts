3d4b0fe7deb678226906e9f9b18fea1cbc9840e0
Guilherme Rosada <guilhermer4@al.insper.edu.br>
2024-10-12 03:17:54 +0100

    edit: change importing

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,14 @@ import { ProductsModule } from './products/products.module';
 import { CompaniesModule } from './companies/companies.module';
 import { CartsModule } from './carts/carts.module';
 import { TypeOrmConfigModule } from './@infrastructure/typeorm/typeorm.module';
+import { OrdersModule } from './orders/orders.module';
 
 @Module({
   imports: [
     TypeOrmConfigModule, 
     ProductsModule, 
     CompaniesModule, 
-    CartsModule
+    CartsModule, OrdersModule
   ],
   controllers: [],
   providers: [],
